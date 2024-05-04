9c2b0ccd267cdf0a71d2536d18d9ec83d7b2e635
Luj8n <luj8n@duck.com>
2024-05-05 00:11:24 +0200

    Refactor into functions

--- src/index.ts
+++ src/index.ts
@@ -1,169 +1,215 @@
-import type { DiffResultTextFile } from "simple-git";
+import type { DefaultLogFields, DiffResultTextFile, LogResult } from "simple-git";
 
 import { simpleGit } from "simple-git";
 import { program } from "commander";
 import fs from "fs";
 
-program
-  .requiredOption("-r, --repository <REPOSITORY>", "link to the git repository. Example: https://github.com/Luj8n/oopp-team-25.git")
-  .option("-t, --threshold [THRESHOLD]", "specify threshold (from 0 to 1). Default = 0.6", parseFloat, 0.6)
+async function main() {
+  program
+    .requiredOption(
+      "-r, --repository <REPOSITORY>",
+      "link to the git repository. Example: https://github.com/Luj8n/oopp-team-25.git"
+    )
+    .option(
+      "-t, --threshold [THRESHOLD]",
+      "specify threshold (from 0 to 1). Default = 0.6",
+      parseFloat,
+      0.6
+    );
 
-program.parse();
-const options = program.opts();
+  program.parse();
+  const options = program.opts();
 
-const REPOSITORY = options.repository;
-const THRESHOLD = options.threshold;
+  const repository = options.repository;
+  const threshold = options.threshold;
 
-function deleteTmp() {
+  try {
+    cleanup();
+    await downloadRepository(repository);
+  } catch (e) {
+    console.error(`Could not download '${repository}': ${e}`);
+    process.exit(1);
+  }
+
+  const commits = await getCommits();
+
+  const { fileData, authors } = processCommits(commits);
+
+  processData(fileData);
+
+  analyzeData(fileData, authors, threshold);
+
+  cleanup();
+}
+
+async function downloadRepository(repository: string) {
+  console.log(`Downloading '${repository}'...`);
+  await simpleGit().clone(repository, "./tmp");
+}
+
+function cleanup() {
   fs.rmSync("./tmp", { recursive: true, force: true });
 }
 
-console.log(`Downloading '${REPOSITORY}'...`);
-try {
-  deleteTmp();
-  await simpleGit().clone(REPOSITORY, "./tmp");
-} catch (e) {
-  console.error(`Could not download '${REPOSITORY}': ${e}`);
-  process.exit(1);
+async function getCommits() {
+  console.log("Getting commits...");
+  const git = simpleGit({ baseDir: "./tmp" });
+  const commits = await git.log([
+    "--numstat", // add commit statistics
+    "--no-merges", // ignore merge commits
+  ]);
+  return commits;
 }
-console.log("Download done");
 
-const git = simpleGit({ baseDir: "./tmp" });
-
-const commits = await git.log([
-  "--numstat", // add commit statistics
-  "--no-merges", // ignore merge commits
-]);
-
-const fileData: {
+interface FileData {
   [file: string]: {
     author: string;
     date: string;
     diff: DiffResultTextFile;
   }[];
-} = {};
-
-const authors: string[] = [];
-
-for (const commit of commits.all) {
-  if (!commit.diff) continue;
-  for (const fileDiff of commit.diff.files) {
-    if (fileDiff.binary) continue;
-    if (fileData[fileDiff.file] == undefined) fileData[fileDiff.file] = [];
-    fileData[fileDiff.file].push({ author: commit.author_name, date: commit.date, diff: fileDiff });
-
-    if (!authors.includes(commit.author_name)) authors.push(commit.author_name);
-  }
 }
 
-const renames = [];
+function processCommits(commits: LogResult<DefaultLogFields>) {
+  const fileData: FileData = {};
 
-for (const file in fileData) {
-  const regex1 = /{(.*?) => (.*?)}/g;
-  if (file.match(regex1)) {
-    const from = file.replace(regex1, "$1").replace(/\/\//g, "/");
-    const to = file.replace(regex1, "$2").replace(/\/\//g, "/");
-    renames.push({ from, to });
+  const authors: string[] = [];
 
-    if (!fileData[to]) fileData[to] = [];
-    fileData[to] = fileData[to].concat(fileData[file]);
-    delete fileData[file];
-    continue;
+  for (const commit of commits.all) {
+    if (!commit.diff) continue;
+    for (const fileDiff of commit.diff.files) {
+      if (fileDiff.binary) continue;
+      if (fileData[fileDiff.file] == undefined) fileData[fileDiff.file] = [];
+      fileData[fileDiff.file].push({
+        author: commit.author_name,
+        date: commit.date,
+        diff: fileDiff,
+      });
+
+      if (!authors.includes(commit.author_name)) authors.push(commit.author_name);
+    }
   }
 
-  const regex2 = /^(.*?) => (.*?)$/g;
-  if (file.match(regex2)) {
-    const from = file.replace(regex2, "$1");
-    const to = file.replace(regex2, "$2");
-    renames.push({ from, to });
-
-    if (!fileData[to]) fileData[to] = [];
-    fileData[to] = fileData[to].concat(fileData[file]);
-    delete fileData[file];
-  }
+  return { fileData, authors };
 }
 
-renames.reverse();
+function processData(fileData: FileData) {
+  console.log("Processing data...");
 
-while (true) {
-  let modified = false;
+  const renames = [];
 
-  for (const rename of renames) {
-    // If the 'from' or 'to' files doesn't have commits then ignore it
-    if (
-      !fileData[rename.from] ||
-      fileData[rename.from].length == 0 ||
-      !fileData[rename.to] ||
-      fileData[rename.to].length == 0
-    )
+  for (const file in fileData) {
+    const regex1 = /{(.*?) => (.*?)}/g;
+    if (file.match(regex1)) {
+      const from = file.replace(regex1, "$1").replace(/\/\//g, "/");
+      const to = file.replace(regex1, "$2").replace(/\/\//g, "/");
+      renames.push({ from, to });
+
+      if (!fileData[to]) fileData[to] = [];
+      fileData[to] = fileData[to].concat(fileData[file]);
+      delete fileData[file];
       continue;
-
-    fileData[rename.to] = fileData[rename.to].concat(fileData[rename.from].splice(0));
-    modified = true;
-  }
-
-  // Only stop trying to move commits if nothing was moved
-  if (!modified) break;
-}
-
-for (const file in fileData) {
-  if (fileData[file].length == 0) delete fileData[file];
-}
-
-console.log("Analyzing data...");
-
-const developerData: {
-  [author: string]: {
-    [file: string]: number;
-  };
-} = {};
-
-for (const developer of authors) {
-  developerData[developer] = {};
-}
-
-for (const file in fileData) {
-  const commitData = fileData[file];
-  for (const commit of commitData) {
-    // TODO: try other ways of calculating weight
-
-    // const commitWeight = Math.sqrt(commit.diff.changes); // Similar if developers have worked on the same files and maybe more on others
-    const commitWeight = commit.diff.changes; // Similar if developers have worked on the same files and maybe a little on others
-    // const commitWeight = commit.diff.changes ** 2; // Similar if developers have worked a lot on the same files and almost nothing more
-    if (commitWeight == 0) continue;
-    if (!developerData[commit.author][file]) developerData[commit.author][file] = 0;
-    developerData[commit.author][file] += commitWeight;
-  }
-}
-
-for (const a of authors) {
-  let aWeightSum = Object.values(developerData[a]).reduce((a, c) => a + c, 0);
-
-  for (const b of authors) {
-    // Don't look at pairs twice
-    if (a >= b) continue;
-
-    let bWeightSum = Object.values(developerData[b]).reduce((a, c) => a + c, 0);
-    const bothWeightSum = aWeightSum + bWeightSum;
-
-    // Ignore developers who didn't make many changes
-    if (bothWeightSum < 100) continue;
-
-    let similarWeightSum = 0;
-    for (const file in developerData[a]) {
-      const aWeight = developerData[a][file];
-      const bWeight = developerData[b][file] ?? 0;
-
-      if (bWeight == 0) continue; // A made changes but B did not
-      else similarWeightSum += aWeight + bWeight; // Both made changes
     }
-    const similarity = similarWeightSum / bothWeightSum;
 
-    if (similarity > THRESHOLD) {
-      console.log(`'${a}' and '${b}': similarity = ${Math.floor(similarity * 100)}%`);
+    const regex2 = /^(.*?) => (.*?)$/g;
+    if (file.match(regex2)) {
+      const from = file.replace(regex2, "$1");
+      const to = file.replace(regex2, "$2");
+      renames.push({ from, to });
+
+      if (!fileData[to]) fileData[to] = [];
+      fileData[to] = fileData[to].concat(fileData[file]);
+      delete fileData[file];
     }
   }
+
+  renames.reverse();
+
+  while (true) {
+    let modified = false;
+
+    for (const rename of renames) {
+      // If the 'from' or 'to' files doesn't have commits then ignore it
+      if (
+        !fileData[rename.from] ||
+        fileData[rename.from].length == 0 ||
+        !fileData[rename.to] ||
+        fileData[rename.to].length == 0
+      )
+        continue;
+
+      fileData[rename.to] = fileData[rename.to].concat(fileData[rename.from].splice(0));
+      modified = true;
+    }
+
+    // Only stop trying to move commits if nothing was moved
+    if (!modified) break;
+  }
+
+  for (const file in fileData) {
+    if (fileData[file].length == 0) delete fileData[file];
+  }
 }
 
-// Cleanup
-deleteTmp();
+function analyzeData(fileData: FileData, authors: string[], threshold: number) {
+  console.log("Analyzing data...");
+
+  const developerData: {
+    [author: string]: {
+      [file: string]: number;
+    };
+  } = {};
+
+  for (const developer of authors) {
+    developerData[developer] = {};
+  }
+
+  for (const file in fileData) {
+    const commitData = fileData[file];
+    for (const commit of commitData) {
+      // TODO: try other ways of calculating weight
+
+      // const commitWeight = Math.sqrt(commit.diff.changes); // Similar if developers have worked on the same files and maybe more on others
+      const commitWeight = commit.diff.changes; // Similar if developers have worked on the same files and maybe a little on others
+      // const commitWeight = commit.diff.changes ** 2; // Similar if developers have worked a lot on the same files and almost nothing more
+      if (commitWeight == 0) continue;
+      if (!developerData[commit.author][file]) developerData[commit.author][file] = 0;
+      developerData[commit.author][file] += commitWeight;
+    }
+  }
+
+  for (const a of authors) {
+    let aWeightSum = Object.values(developerData[a]).reduce((a, c) => a + c, 0);
+
+    for (const b of authors) {
+      // Don't look at pairs twice
+      if (a >= b) continue;
+
+      let bWeightSum = Object.values(developerData[b]).reduce((a, c) => a + c, 0);
+      const bothWeightSum = aWeightSum + bWeightSum;
+
+      // Ignore developers who didn't make many changes
+      if (bothWeightSum < 100) continue;
+
+      let similarWeightSum = 0;
+      for (const file in developerData[a]) {
+        const aWeight = developerData[a][file];
+        const bWeight = developerData[b][file] ?? 0;
+
+        if (bWeight == 0) {
+          // A made changes but B did not
+          continue;
+        } else {
+          // Both made changes
+          similarWeightSum += aWeight + bWeight;
+        }
+      }
+      const similarity = similarWeightSum / bothWeightSum;
+
+      if (similarity > threshold) {
+        console.log(`'${a}' and '${b}': similarity = ${Math.floor(similarity * 100)}%`);
+      }
+    }
+  }
+}
+
+await main();
